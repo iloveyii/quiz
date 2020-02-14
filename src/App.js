6047ba96eb021af372924706ddb72573dab55e3e
Hazrat Ali <iloveyii@yahoo.com>
2020-02-14 11:31:42 +0100

    Added sticky footer theme

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary">
+            {'Copyright © '}
+            <Link color="inherit" href="https://material-ui.com/">
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
 }
 
-export default App;
+const styles = theme => ({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+    },
+    main: {
+        marginTop: theme.spacing(8),
+        marginBottom: theme.spacing(2),
+    },
+    footer: {
+        padding: theme.spacing(3, 2),
+        marginTop: 'auto',
+        backgroundColor:
+            theme.palette.type === 'dark' ? theme.palette.grey[800] : theme.palette.grey[200],
+    },
+});
+
+
+class App extends React.Component {
+
+    render() {
+        const {classes} = this.props;
+
+        return (
+            <div className={classes.root}>
+                <CssBaseline/>
+                <Container component="main" className={classes.main} maxWidth="sm">
+                    <Typography variant="h2" component="h1" gutterBottom>
+                        Sticky footer
+                    </Typography>
+                    <Typography variant="h5" component="h2" gutterBottom>
+                        {'Pin a footer to the bottom of the viewport.'}
+                        {'The footer will move as the main element of the page grows.'}
+                    </Typography>
+                    <Typography variant="body1">Sticky footer placeholder.</Typography>
+                </Container>
+                <footer className={classes.footer}>
+                    <Container maxWidth="sm">
+                        <Typography variant="body1">My sticky footer can be found here.</Typography>
+                        <Copyright/>
+                    </Container>
+                </footer>
+            </div>
+        );
+    }
+}
+
+export default withStyles(styles)(App);
+
+
